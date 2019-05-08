348fe643d8287415a73171126c41d819fcb6fc57
Andrew Donnellan <andrew@donnellan.id.au>
2019-05-08 13:09:42 +1000

    Move settings out to separate file
    Signed-off-by: Andrew Donnellan <andrew@donnellan.id.au>

--- twitterbot.py
+++ twitterbot.py
@@ -9,29 +9,7 @@ import re
 import sys
 import time
 from datetime import date
-
-class Settings:
-	"""
-	Twitter bot application settings.
-	
-	Enter the RSS feed you want to tweet, or keywords you want to retweet.
-	"""
-	FeedUrl = "http://example.net/feed/"               # RSS feed to read and post tweets from.
-	PostedUrlsOutputFile = "posted-urls.log"           # Log file to save all tweeted RSS links (one URL per line).
-	PostedRetweetsOutputFile = "posted-retweets.log"   # Log file to save all retweeted tweets (one tweetid per line).
-	RetweetIncludeWords = ["#hashtag"]                 # Include tweets with these words when retweeting.
-	RetweetExcludeWords = []                           # Do not include tweets with these words when retweeting.
-
-class TwitterAuth:
-	"""
-	Twitter authentication settings.
-
-	Create a Twitter app at https://apps.twitter.com/ and generate key, secret etc. and insert them here.
-	"""
-	ConsumerKey = "XXX"
-	ConsumerSecret = "XXX"
-	AccessToken = "XXX"
-	AccessTokenSecret = "XXX"
+from twitterbot_settings import Settings, TwitterAuth
 
 def compose_message(rss_item):
 	"""Compose a tweet from title, link, and description, and then return the final tweet message."""
---
+++ twitterbot_settings.py
@@ -0,0 +1,24 @@
+# This file is gitignored by default
+
+class Settings:
+	"""
+	Twitter bot application settings.
+	
+	Enter the RSS feed you want to tweet, or keywords you want to retweet.
+	"""
+	FeedUrl = "http://example.net/feed/"		   # RSS feed to read and post tweets from.
+	PostedUrlsOutputFile = "posted-urls.log"	   # Log file to save all tweeted RSS links (one URL per line).
+	PostedRetweetsOutputFile = "posted-retweets.log"   # Log file to save all retweeted tweets (one tweetid per line).
+	RetweetIncludeWords = ["#hashtag"]		   # Include tweets with these words when retweeting.
+	RetweetExcludeWords = []			   # Do not include tweets with these words when retweeting.
+
+class TwitterAuth:
+	"""
+	Twitter authentication settings.
+
+	Create a Twitter app at https://apps.twitter.com/ and generate key, secret etc. and insert them here.
+	"""
+	ConsumerKey = "XXX"
+	ConsumerSecret = "XXX"
+	AccessToken = "XXX"
+	AccessTokenSecret = "XXX"
